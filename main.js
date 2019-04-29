c565908b334ccda441951319849b5154b6951085
Vincent <34786079+Cloud8PlayZ@users.noreply.github.com>
2019-04-29 15:33:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var rgb1 = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 var rgb2 = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+var rgb3 = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
 function setup(){
   createCanvas(400,400);
